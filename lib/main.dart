3b52f8a2b540fc8f0da665d036da675e8b48147d
Aaliashehzadi433 <ayezo433@gmail.com>
2023-01-19 22:43:55 -0800

    changed title namae

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase project',
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
